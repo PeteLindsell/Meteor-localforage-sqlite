06a2af80e025ba3625ed4183f46afa3d22ee20f6
Morten Henriksen <mh@gi2.dk>
2015-08-25 02:17:45 +0200

    1.2.4-rc.1 Meteor 1.2-rc.4 test

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'raix:localforage-sqlite',
-  version: '1.0.0',
+  version: '1.0.0-rc.1',
   summary: "Offline storage driver SQL lite for cordova"
 });
 
@@ -10,11 +10,11 @@ Cordova.depends({
 });
 
 Package.on_use(function (api) {
-  // api.versionsFrom('1.2');
+  api.versionsFrom('METEOR@1.2-rc.4');
   api.use('ecmascript', 'web.cordova');
 
   // Depends on the serializer
-  api.use('raix:localforage-serializer@1.2.4', 'web.cordova');
+  api.use('raix:localforage-serializer@1.2.4-rc.1', 'web.cordova');
 
   api.addFiles('sqlite.js', 'web.cordova');
   api.export('cordovaSqliteplugin', 'web.cordova');
